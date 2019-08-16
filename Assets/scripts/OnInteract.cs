cd529b49aece0cb1efeba17990038da3c96b8921
cristol grosdemouge <cristol.gdm@gmail.com>
2019-08-16 12:58:48 +0900

    Pass dialog to text box

--- Assets/scripts/OnInteract.cs
+++ Assets/scripts/OnInteract.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class OnInteract : MonoBehaviour {
 
-	public virtual void StartInteraction() {
+    public Text dialogBoxText;
+    public Image dialogBoxBackground;
+
+    private void Start() {
+        dialogBoxText = GameObject.Find(ComponentNames.DialogBoxName).GetComponentInChildren<Text>();
+        dialogBoxBackground = GameObject.Find(ComponentNames.DialogBoxName).GetComponentInChildren<Image>();
+    }
+    public virtual void StartInteraction() {
         GameData.PlayerCanMove = true;
     }
 }
--- Assets/scripts/OnTalkedTo.cs
+++ Assets/scripts/OnTalkedTo.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OnTalkedTo : OnInteract {
 
     public string dialog = "...";
 
     public override void StartInteraction() {
-        Debug.Log(dialog);
+        dialogBoxText.text = dialog;
         GameData.PlayerCanMove = true;
     }
 }
