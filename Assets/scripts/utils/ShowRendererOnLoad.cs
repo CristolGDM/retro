e553cd5f1ea5622a613d9d6f6bde68e6b9ff3d99
cristol grosdemouge <cristol.gdm@gmail.com>
2019-08-22 11:37:27 +0900

    Small utils script to start showing UI stuff only on load (else annoying during development)

---
+++ Assets/scripts/utils/ShowRendererOnLoad.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ShowRendererOnLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<TilemapRenderer>().enabled = true;
+	}
+
+}
