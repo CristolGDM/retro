5a5ba43008ba06da778b2df75d1899ae492ee1e4
cristol grosdemouge <cristol.gdm@gmail.com>
2019-08-24 12:56:37 +0900

    Add display for PC1

--- Assets/scripts/global/MenuManager.cs
+++ Assets/scripts/global/MenuManager.cs
@@ -21,16 +21,21 @@ public class MenuManager : MonoBehaviour {
         leftPart.GetComponent<TilemapRenderer>().enabled = true;
         rightPart.GetComponent<TilemapRenderer>().enabled = true;
         mainMenu.transform.Find("GameMenuText").gameObject.GetComponent<Canvas>().enabled = true;
+        mainMenu.transform.Find("pc1sprite").gameObject.GetComponent<SpriteRenderer>().enabled = true;
         GameData.MenuIsOpen = true;
     }
     public void closeMenu() {
         TilemapRenderer[] allRenderers = mainMenu.GetComponentsInChildren<TilemapRenderer>();
         Canvas[] allText = mainMenu.GetComponentsInChildren<Canvas>();
+        SpriteRenderer[] allSprites = mainMenu.GetComponentsInChildren<SpriteRenderer>();
         foreach (TilemapRenderer childRenderer in allRenderers) {
             childRenderer.enabled = false;
         }
         foreach (Canvas text in allText) {
             text.enabled = false;
+        }
+        foreach (SpriteRenderer sprite in allSprites) {
+            sprite.enabled = false;
         }
         GameData.MenuIsOpen = false;
     }
