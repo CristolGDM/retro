c1d081319134d4c59b3fae3a3c3691d5c072c099
cristol grosdemouge <cristol.gdm@gmail.com>
2019-12-28 19:30:46 +0900

    Added component to display gold

---
+++ Assets/scripts/menus/GoldComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldComponent : MonoBehaviour {
+
+    [SerializeField]
+    Text goldAmount;
+    	
+	// Update is called once per frame
+	void Update () {
+        goldAmount.text = "" + Inventory.GetGold();
+	}
+}
